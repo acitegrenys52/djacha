426aa63158f8c2a2fa203a9f537c5397a126c30b
Artem <a.homenko@synergetica.net>
2016-03-11 13:11:36 +0200

    1) awsgi added 2) readme added

--- djacha/asgi.py
+++ djacha/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from channels.asgi import get_channel_layer
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djacha.settings")
 
-application = get_wsgi_application()
+channel_layer = get_channel_layer()
